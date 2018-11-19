2dfa2d7f08f2a7085c305c1b2c1f3e99af1231f2
Matt Moore <mattmoor@google.com>
2018-11-19 04:54:00 +0000

    Drop the large request dumper.
    It was useful for debugging, but no longer necessary.

--- pkg/github/handler.go
+++ pkg/github/handler.go
@@ -11,34 +11,6 @@ import (
 	"github.com/google/go-github/github"
 )
 
-// formatRequest generates ascii representation of a request
-// from: https://medium.com/doing-things-right/pretty-printing-http-requests-in-golang-a918d5aaa000
-func formatRequest(r *http.Request) string {
-	// Create return string
-	var request []string
-	// Add the request string
-	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
-	request = append(request, url)
-	// Add the host
-	request = append(request, fmt.Sprintf("Host: %v", r.Host))
-	// Loop through headers
-	for name, headers := range r.Header {
-		name = strings.ToLower(name)
-		for _, h := range headers {
-			request = append(request, fmt.Sprintf("%v: %v", name, h))
-		}
-	}
-
-	// If this is a POST, add post data
-	if r.Method == "POST" {
-		r.ParseForm()
-		request = append(request, "\n")
-		request = append(request, r.Form.Encode())
-	}
-	// Return the request as a string
-	return strings.Join(request, "\n")
-}
-
 func Handler(w http.ResponseWriter, r *http.Request) {
 	payload, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -56,7 +28,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 
 	event, err := github.ParseWebHook(eventType, payload)
 	if err != nil {
-		log.Printf("ERROR: unable to parse webhook: %v\n%v", err, formatRequest(r))
+		log.Printf("ERROR: unable to parse webhook: %v", err)
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
